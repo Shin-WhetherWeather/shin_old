487cd8f00c579d29c9119eb1d4a77e5b7bb5394b
Shin-WhetherWeather <shin.whetherweather@gmail.com>
2023-07-17 23:36:08 +1000

    mobile update

--- textData.js
+++ textData.js
@@ -1,19 +1,19 @@
 let textData = 
 {
     ToI:{
-        titleText: "Project\xa0ToI\xa0",
+        titleText: "Project ToI ",
         bodyText: "ToI is a series of home appliances investigating our relationships with data and technology: ToI want to keep us connected without the associated anxiety and emotional baggage. ToI requires minimal amounts of direct interactions with human users, adopting a non-interfering approach to inform us of the worlds we live in.",
         tagText: "IoT, Microcontrollers, Relationships",
         readMore: "Read More",
     },
     WWClock:{
-        titleText: "Whether\xa0Weather\xa0Clock\xa0",
+        titleText: "Whether Weather Clock ",
         bodyText: "The Whether Weather Clock is a project to inspire different feelings and emotions that accompany us throughout different times of the day.",
         tagText: "Weather, Mood, Time",
         readMore: "Read More",
     },
     Earphones:{
-        titleText: "XHN-22\xa0",
+        titleText: "XHN-22 ",
         bodyText: "XHN-22 is a pair of earphones designed for fitness enthusiasts. It is a pair of stylish, compact earphones with intuitive controls. XHN-22 uses capacitive sensors to detect finger gestures on the bright red central groove. Swipes correspond to volume adjustments, double swipes skip songs, and taps pause the media. These allow for rapid controls for the athlete on the run while requiring minimal cognitive load.",
         tagText: "Industrial Design, TWS earbuds",
         readMore: "Read More",
